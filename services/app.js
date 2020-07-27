f5c06321d47e838e0ca1a413c49070808e3a658f
alissonarm <alissonaugusto@yahoo.com>
2020-07-26 21:48:16 -0300

    inluindo mensagens de validacao

--- services/app.js
+++ services/app.js
@@ -1,11 +1,16 @@
 import { validarInput } from "./validar.js";
 
 window.onload = () => {
+    console.log('estou no app');
     const inputs = document.querySelectorAll("input");
 
     inputs.forEach(input => {
         console.log(input);
         input.addEventListener('input', () => {
+                validarInput(input, false);
+        });
+
+        input.addEventListener('blur', () => {
                 validarInput(input);
         });
     });
--- services/validar.js
+++ services/validar.js
@@ -2,8 +2,33 @@ import { validarDataNascimento } from "./validarDataNascimento";
 
 import { validarDataNascimento } from "./validarDataNascimento.js";
 
-export const validarInput =(input) => {
-        if (input.id === "dataNascimento"){
-            validarDataNascimento();
+export const validarInput = (input, adicionarErro = true) => {
+    const classeElementoErro = "erro-validacao";
+    const classeInputErro = "possui-erro-validacao";
+    const elementoPai = input.parentNode;
+    const elementoErroExiste = elementoPai.querySelector(
+        `.${classeElementoErro}`
+    );
+    const elementoErro = elementoErroExiste || document.createElement('div');
+    const elementoEhValido = input.validity.valid;
+    const tipo = input.dataset.tipo;
+    const validadoresEspecificos = {
+        dataNascimento: input => validarDataNacimento(input)
+    };
+    if (validadoresEspecificos[tipo]) {
+        validadoresEspecificos[tipo](input);
+    }
+
+    if (!elementoEhValido) {
+        elementoErro.className = classeElementoErro;
+        elementoErro.textContent = "Há um erro aqui!";
+        if (adicionaErro){
+            input.after(elementoErro);
+            input.classList.add(classeInputErro);
         }
+    } else {
+        elementoErro.remove();
+        input.classList.remove(classeInputErro);
+
+    }
 };
